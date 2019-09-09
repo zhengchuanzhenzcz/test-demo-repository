cc82aadb20e252e22c4c6dbaa344a44e0562a95e
zhengchuanzhenzcz <18325401017@163.com>
2019-09-09 17:55:25 +0800

    test_repository：测试创建git库和idea关联

---
+++ src/main/java/com/zcz/test_repository/TestRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.zcz.test_repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestRepositoryApplication.class, args);
+    }
+
+}
